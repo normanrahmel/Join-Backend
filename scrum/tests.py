063ec8ff3ca234a3b887fa1f1146b9309e1579c8
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-10-18 15:08:08 +0200

    testing

--- scrum/tests.py
+++ scrum/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test import Client
+from django.test import TestCase
+import unittest
+from django.contrib.auth.models import User
+from django.test import Client
 
-# Create your tests here.
+
+class URLTests(TestCase):
+    def test_testhomepage(self):
+        self.client = Client()
+        self.user = User.objects.create_user('testuser', password='test_user')
+        self.client.login(username='testuser', password='test_user')
+
+        response = self.client.get("/chat/")
+        self.assertEqual(response.status_code, 200)
